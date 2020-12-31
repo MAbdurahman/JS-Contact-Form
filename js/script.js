a7c911a4cab240968d08db0d233cddd351bd50cf
MAbdurahman <mdbdrrhm@gmail.com>
2020-12-31 09:07:52 -0600

    Update index.html and script.js to perform validation.  Need to make apply to phone input field

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
            JS-Contact-Form Scripts
 ============================================ */
 $(document).ready(function () {
+
     const green = '#335536';
     const red = '#9a0220';
 
@@ -15,7 +16,9 @@ $(document).ready(function () {
     let isEmailValid = false;
     let isMessageValid = false;
 
-    //getPrompt function - produces the message at the element id in the specific color
+    /**
+     * getPrompt Function - produces the message at the element id in the specific color
+     */
     function getPrompt(message, prompt_location, color) {
         document.getElementById(prompt_location).innerHTML = message;
         document.getElementById(prompt_location).style.color = color;
@@ -28,10 +31,10 @@ $(document).ready(function () {
      */
     function checkNameInput() {
 
-        let name = $('#contact__form--name').val();
+        let name = $('#icon_prefix').val();
         let message = "";
 
-        if (name.length == 0) {
+        if (name.length === 0) {
             message = "Your first and last name is required!";
             isNameValid = false;
             getPrompt(message, "contact__form--name-prompt", red);
@@ -59,12 +62,12 @@ $(document).ready(function () {
      * in the specified input field.
      */
     function checkPhoneInput() {
-        var pattern = /^\D?(\d{3})\D?\D?(\d{3})\D?(\d{4})$/;
-        //var pattern2 = /^[0-9]{10}$/;
-        var message = "";
-        var phone = document.getElementById("contact__form--phone-prompt").value;
+        let pattern = /^\D?(\d{3})\D?\D?(\d{3})\D?(\d{4})$/;
+        let message = "";
+        let phone = $('#icon_telephone').val();
+        // let phone = document.getElementById("icon_telephone").value;
 
-        if (phone.length == 0) {
+        if (phone.length === 0) {
             message = "Your phone number is required!";
             getPrompt(message, "contact__form--phone-prompt", "red");
             return false;
@@ -77,15 +80,18 @@ $(document).ready(function () {
         message = "Valid phone number";
         getPrompt(message, "contact__form--phone-prompt", "green");
         return true;
-    }
+    }//end of the checkPhoneInput Function
 
-    //checkEmailInput Function - checks the email input field
+    /**
+     * checkEmailInput function - Validates that user has entered a valid email in the
+     * specified input field.
+     */
     function checkEmailInput() {
 
-        let email = $('#contact__form--email').val();
+        let email = $('#email').val();
         let message = "";
 
-        if (email.length == 0) {
+        if (email.length === 0) {
 
             message = "Your email address is required!";
             isEmailValid = false;
@@ -110,10 +116,13 @@ $(document).ready(function () {
 
     } //end of the checkEmailInput Function
 
-    //checkMessageInput Function - checks the message input field
+    /**
+     * checkMessageInput function - Validates that user has entered a valid message with the
+     * required characters in the specified input field.
+     */
     function checkMessageInput() {
 
-        let form_message = $("#contact__form--message").val();
+        let form_message = $("#textarea1").val();
         let characters_left = (required_message_length - form_message.length);
         let message = "";
 
@@ -136,7 +145,11 @@ $(document).ready(function () {
 
     } //end of the checkMessageInput Function
 
-    //performValidForm Function -
+
+    /**
+     * performValidForm function - Validates that user has entered a valid input in all
+     * of the input fields and textarea.
+     */
     function performValidForm() {
 
         $('#contact__form--submit').val('Valid Form');
@@ -149,84 +162,127 @@ $(document).ready(function () {
 
         $('#contact__form--submit').prop('disable', true);
 
+
         if ($('#contact__form--submit').hasClass('valid')) {
             setTimeout(function () {
                 $('#contact__form--submit').val('Message Sent');
                 $('#success__message').show(1000);
             }, 3000);
+
+            resetForm();
         }
-    } //end of the performValidForm Function
 
-    //performInvalidForm Function -
-    function performInvalidForm() {
+        } //end of the performValidForm Function
 
-        $('#contact__form--submit').val('Check Form & Click Again!');
-        $('#contact__form--submit').removeClass('valid');
-        updateErrors();
-        $('#success__message').hide(250);
-        $('#error__message').show(1250);
-        $('#error__message').effect('shake', {
-            times: 5
-        }, 1000);
 
-    } //end of the performInvalidForm Function
+        /**
+         * performInvalidForm function - performs invalid message when user has entered
+         * invalid input in any of  input fields and textarea.
+         */
+        function performInvalidForm() {
 
-    //updateErrors Function -
-    function updateErrors() {
-        let message = "";
-        $('#error__message').html(`<h4>Error!</h4>
+            $('#contact__form--submit').val('Check Form & Click Again!');
+            $('#contact__form--submit').removeClass('valid');
+            updateErrors();
+            $('#success__message').hide(250);
+            $('#error__message').show(1250);
+            $('#error__message').effect('shake', {
+                times: 5
+            }, 1000);
+
+        } //end of the performInvalidForm Function
+
+        /**
+         * updateErrors function - update the message in the error message id when user
+         * has entered invalid input in any input fields and textarea.
+         */
+        function updateErrors() {
+            let message = "";
+            $('#error__message').html(`<h4>Error!</h4>
                   <p>The following are error(s) in the form:</p>`);
 
-        if (!isNameValid) {
-            if ($('#contact__form--name').val().length == 0) {
-                $('#error__message').append(`<p>Your first and last name is required!!</p>`);
-                message = "Your first and last name is required!";
-                getPrompt(message, "contact__form--name-prompt", red);
+            if (!isNameValid) {
+                if ($('#icon_prefix').val().length === 0) {
+                    $('#error__message').append(`<p>Your first and last name is required!!</p>`);
+                    message = "Your first and last name is required!";
+                    getPrompt(message, "contact__form--name-prompt", red);
 
-            } else {
-                $('#error__message').append(`<p>Enter first and last name only!!</p>`);
-                message = "Enter first and last name only!";
-                getPrompt(message, "contact__form--name-prompt", red);
+                } else {
+                    $('#error__message').append(`<p>Enter first and last name only!!</p>`);
+                    message = "Enter first and last name only!";
+                    getPrompt(message, "contact__form--name-prompt", red);
+
+                }
+            }
+            if (!isPhoneValid) {
+                if ($('#icon_telephone').val().length === 0) {
+
+
+                } else {
+
+
+                }
+            }
+            if (!isEmailValid) {
+                if ($('#email').val().length === 0) {
+                    $('#error__message').append(`<p>Your email address is required!!</p>`);
+                    message = "Your email address is required!";
+                    getPrompt(message, "contact__form--email-prompt", red);
+
+                } else {
+                    $('#error__message').append(`<p>Your email address is Invalid!!</p>`);
+                    message = "Invalid email address!";
+                    getPrompt(message, "contact__form--email-prompt", red);
+                }
+            }
+            if (!isMessageValid) {
+                let characters_left = (required_message_length - $('#textarea1').val().length);
+                let messageData = characters_left + " more characters required in message!!";
+
+                $('#error__message').append(`<p>${messageData}</p>`);
+                message = characters_left + " more characters required in message!";
+                getPrompt(message, "contact__form--message-prompt", red);
 
             }
-        }
-        if (!isEmailValid) {
-            if ($('#contact__form--email').val().length == 0) {
-                $('#error__message').append(`<p>Your email address is required!!</p>`);
-                message = "Your email address is required!";
-                getPrompt(message, "contact__form--email-prompt", red);
+        } //end of the updateErrors Function
+
+        //checkFormValidation Function -
+        function checkFormValidation() {
+            if (isNameValid && isEmailValid && isMessageValid) {
+                performValidForm();
 
             } else {
-                $('#error__message').append(`<p>Your email address is Invalid!!</p>`);
-                message = "Invalid email address!";
-                getPrompt(message, "contact__form--email-prompt", red);
+                performInvalidForm();
+
             }
+        } //end of the checkFormValidation Function
+
+        function resetForm() {
+            let message = null;
+            let color = null;
+
+            $('#contact__form input[type="text"]').val('').focus(null).removeClass('focus-visible');
+            getPrompt(message, "contact__form--name-prompt", color);
+            $('#contact__form #name__label').removeClass('active');
+
+            $('#contact__form input[type="tel"]').val('').focus(null).removeClass('focus-visible');
+            getPrompt(message, "contact__form--phone-prompt", color);
+            $('#contact__form #phone__label').removeClass('active');
+
+            $('#contact__form input[type="email"]').val('').focus(null).removeClass('focus-visible');
+            getPrompt(message, "contact__form--email-prompt", color);
+            $('#contact__form #email__label').removeClass('active');
+
+            $('#contact__form #textarea1').val('').focus(null).removeClass('focus-visible');
+            getPrompt(message, "contact__form--message-prompt", color);
+            $('#contact__form #message__label').removeClass('active');
+
         }
-        if (!isMessageValid) {
-            let characters_left = (required_message_length - $('#contact__form--message').val().length);
-            let messageData = characters_left + " more characters required in message!!";
 
-            $('#error__message').append(`<p>${messageData}</p>`);
-            message = characters_left + " more characters required in message!";
-            getPrompt(message, "contact__form--message-prompt", red);
-
-        }
-    } //end of the updateErrors Function
-
-    //checkFormValidation Function -
-    function checkFormValidation() {
-        if (isNameValid && isEmailValid && isMessageValid) {
-            performValidForm();
-
-        } else {
-            performInvalidForm();
-
-        }
-    } //end of the checkFormValidation Function
-
-    $('#contact__form--name').keyup(checkNameInput);
-    $('#contact__form--email').keyup(checkEmailInput);
-    $('#contact__form--message').keyup(checkMessageInput);
-    $('#contact__form--submit').click(checkFormValidation);
+        $('#icon_prefix').keyup(checkNameInput);
+        $('#icon_telephone').keyup(checkPhoneInput);
+        $('#email').keyup(checkEmailInput);
+        $('#textarea1').keyup(checkMessageInput);
+        $('#contact__form--submit').click(checkFormValidation);
 
 });
